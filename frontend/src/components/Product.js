f67f411a26a38321e0afd4c139450e2f5446ac8f
Enis Hamza <eniss.hamza@gmail.com>
2024-11-14 15:52:12 +0100

    Routes

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,11 +6,11 @@ import Rating from "./Rating";
 const Product = ({ product }) => {
   return (
     <Card className="my-2 p-3 rounded">
-      <a href={`https://shop115.onrender.com/product/${product._id}`}>
+      <a href={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
       </a>
       <Card.Body>
-        <Link to={`https://shop115.onrender.com/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
