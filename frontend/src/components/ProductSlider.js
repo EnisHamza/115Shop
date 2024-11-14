da8aae522110eeea202e971db58bacfb379d9de2
Enis Hamza <eniss.hamza@gmail.com>
2024-11-14 17:34:48 +0100

    Slider component

--- frontend/src/components/ProductSlider.js
+++ frontend/src/components/ProductSlider.js
@@ -25,7 +25,13 @@ const ProductSlider = () => {
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
+            {/* Image component is fluid, which should make it responsive */}
+            <Image
+              src={product.image}
+              alt={product.name}
+              fluid
+              className="carousel-image"
+            />
             <Carousel.Caption className="carousel-caption">
               <h2>
                 {product.name} (${product.price})
