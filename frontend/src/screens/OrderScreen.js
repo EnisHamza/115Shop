fc34ff368aad34d24c06c80c716ddcfe3eda3cd7
Enis Hamza <eniss.hamza@gmail.com>
2024-11-14 16:38:10 +0100

    Route Url

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -47,7 +47,9 @@ const OrderScreen = ({ match }) => {
 
   useEffect(() => {
     const addPayPalScript = async () => {
-      const { data: clientId } = await axios.get("/api/config/paypal");
+      const { data: clientId } = await axios.get(
+        "https://shop115-backend.onrender.com/api/config/paypal"
+      );
       const script = document.createElement("script");
       script.type = "text/javascript";
       script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`;
